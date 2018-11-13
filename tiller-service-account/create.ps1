1be74d18484f1e41cf3aad32d43d5eefd5db068d
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-11-13 12:54:12 +0000

    Create tiller service account

---
+++ tiller-service-account/create.ps1
@@ -0,0 +1,27 @@
+# .\create.ps1 -namespace 'ummati-production'
+
+[CmdletBinding()]
+Param(
+    [string]$namespace
+)
+
+Write-Output "Creating Namespace $namespace";
+kubectl create namespace $namespace
+Write-Output '';
+
+Write-Output "Creating Service Account tiller-$namespace";
+kubectl create serviceaccount "tiller-$namespace" --namespace $namespace
+Write-Output '';
+
+Write-Output 'Creating Role';
+(Get-Content 'role.yaml').replace('$namespace', $namespace) | Set-Content 'role-output.yaml';
+Get-Content role-output.yaml
+Write-Output '';
+kubectl apply --filename role-output.yaml
+Write-Output '';
+
+Write-Output 'Creating Role Binding';
+(Get-Content 'role-binding.yaml').replace('$namespace', $namespace) | Set-Content 'role-binding-output.yaml';
+Get-Content role-binding-output.yaml
+Write-Output '';
+kubectl apply --filename role-binding-output.yaml
---
+++ tiller-service-account/create.sh
@@ -0,0 +1,24 @@
+# .\create.sh "ummati-production"
+
+namespace="$1"
+
+echo "Creating Namespace $namespace"
+kubectl create namespace $namespace
+echo
+
+echo "Creating Service Account tiller-$namespace"
+kubectl create serviceaccount "tiller-$namespace" --namespace $namespace
+echo
+
+echo 'Creating Role'
+sed 's/$namespace/foo/g' <role.yaml >role-output.yaml
+cat role-output.yaml
+echo
+kubectl apply --filename role-output.yaml
+echo
+
+echo 'Creating Role Binding'
+sed 's/$namespace/foo/g' <role-binding.yaml >role-binding-output.yaml
+cat role-binding-output.yaml
+echo
+kubectl apply --filename role-binding-output.yaml
