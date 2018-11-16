d535074f4097cb0ae523f49badf4aa7cedd25ca5
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2018-11-16 09:22:06 +0000

    formatting

--- tiller-service-account/create.ps1
+++ tiller-service-account/create.ps1
@@ -1,4 +1,4 @@
-# .\create.ps1 -Namespace 'ummati-production'
+# .\create.ps1 -Namespace 'foo'
 
 [CmdletBinding()]
 Param(
@@ -6,16 +6,24 @@ Param(
 )
 
 Write-Output "Creating Namespace $Namespace";
-kubectl create namespace $Namespace
+kubectl create namespace $Namespace --output yaml
 Write-Output '';
 
 Write-Output "Creating Service Account tiller-$Namespace";
-kubectl create serviceaccount "tiller-$Namespace" --namespace $Namespace
+kubectl create serviceaccount "tiller-$Namespace" --namespace $Namespace --output yaml
 Write-Output '';
 
 Write-Output "Creating Role tiller-role-$Namespace"
-kubectl create role "tiller-role-$Namespace" --namespace $Namespace --verb=* --resource=*.,*.apps,*.batch,*.extensions
+kubectl create role "tiller-role-$Namespace" `
+    --namespace $Namespace `
+    --verb=* `
+    --resource=*.apps,*.batch,*.extensions,*. `
+    --output yaml
 Write-Output '';
 
 Write-Output "Creating Role Binding tiller-rolebinding-$Namespace"
-kubectl create rolebinding "tiller-rolebinding-$Namespace" --namespace $Namespace --role="tiller-role-$Namespace" --serviceaccount="$Namespace`:tiller-$Namespace"
+kubectl create rolebinding "tiller-rolebinding-$Namespace" `
+    --namespace $Namespace `
+    --role="tiller-role-$Namespace" `
+    --serviceaccount="$Namespace`:tiller-$Namespace" `
+    --output yaml
--- tiller-service-account/create.sh
+++ tiller-service-account/create.sh
@@ -1,18 +1,26 @@
-# .\create.sh "ummati-production"
+# .\create.sh "foo"
 
 Namespace="$1"
 
 echo "Creating Namespace $Namespace"
-kubectl create namespace $Namespace
+kubectl create namespace $Namespace --output yaml
 echo
 
 echo "Creating Service Account tiller-$Namespace"
-kubectl create serviceaccount "tiller-$Namespace" --namespace $Namespace
+kubectl create serviceaccount "tiller-$Namespace" --namespace $Namespace --output yaml
 echo
 
 echo 'Creating Role tiller-role-$Namespace'
-kubectl create role "tiller-role-$Namespace" --namespace $Namespace --verb=* --resource=*.,*.apps,*.batch,*.extensions
+kubectl create role "tiller-role-$Namespace" /
+    --namespace $Namespace /
+    --verb=* /
+    --resource=*.,*.apps,*.batch,*.extensions /
+    --output yaml
 echo
 
 echo 'Creating Role Binding tiller-rolebinding-$Namespace'
-kubectl create rolebinding "tiller-rolebinding-$Namespace" --namespace $Namespace --role="tiller-role-$Namespace" --serviceaccount="$Namespace:tiller-$Namespace"
+kubectl create rolebinding "tiller-rolebinding-$Namespace" /
+    --namespace $Namespace /
+    --role="tiller-role-$Namespace" /
+    --serviceaccount="$Namespace:tiller-$Namespace" /
+    --output yaml
